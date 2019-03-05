daec1902c41c0b22964da51c7327823047001e7c
Dorian FAURE <E14A446W@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-05 17:32:12 +0100

    ajout animal

--- index.cob
+++ index.cob
@@ -24,7 +24,7 @@
 
         SELECT femployes ASSIGN TO "employes.dat"
         ORGANIZATION INDEXED
-        RECORD KEY fem_numS
+        RECORD KEY fem_numEmp
         ALTERNATE RECORD KEY fem_dateEmbauche with duplicates
         ACCESS MODE DYNAMIC
         FILE STATUS is femplCR.
@@ -38,9 +38,9 @@
 
         SELECT fsoins ASSIGN TO "soins.dat"
         ORGANIZATION INDEXED
-        RECORD KEY fv_numS
-        ALTERNATE RECORD KEY fv_descriptif with duplicates
-        ALTERNATE RECORD KEY fv_numA with duplicates
+        RECORD KEY fs_numS
+        ALTERNATE RECORD KEY fs_descriptif with duplicates
+        ALTERNATE RECORD KEY fs_numA with duplicates
         ACCESS MODE DYNAMIC
         FILE STATUS is fsoinCR.
 
@@ -65,7 +65,7 @@
                         02 fa_dernierRepas pic 9(9).
                         02 fa_dernierVaccin pic 9(9).
 
-        FD femployes
+        FD femployes.
                 01 empl_tamp.
                         02 fem_numEmp pic 9(4).
                         02 fem_nom pic A(30).
@@ -75,11 +75,11 @@
                         02 fem_telephone pic 9(10).
                         02 fem_type pic A(20).
 
-        FD frepas
-                01 repa_tamp
+        FD frepas.
+                01 repa_tamp.
                         02 fr_numR pic 9(9).
                         02 fr_description pic A(999).
-                        02 fr_jour pic 9.
+                        02 fr_jour pic 9(2).
                         02 fr_mois pic 9(2).
                         02 fr_annee pic 9(4).
                         02 fr_heure pic 9(4).
@@ -87,14 +87,14 @@
                         02 fr_numAnimal pic 9(3).
                         02 fr_prixRepas pic 9(4).
 
-        FD fsoins
-                01 soin_tamp
-                        02 fv_numS pic 9(4).
-                        02 fv_descriptif pic A(20).
-                        02 fv_date pic 9(8).
-                        02 fv_numMedecin pic A(30).
-                        02 fv_type pic A(30).
-                        02 fv_numA pic 9(4).
+        FD fsoins.
+                01 soin_tamp.
+                        02 fs_numS pic 9(4).
+                        02 fs_descriptif pic A(20).
+                        02 fs_date pic 9(8).
+                        02 fs_numMedecin pic A(30).
+                        02 fs_type pic A(30).
+                        02 fs_numA pic 9(4).
 
         WORKING-STORAGE SECTION.
                 77 fenclCR pic 9(2).
@@ -103,4 +103,78 @@
                 77 frepaCR pic 9(2).
                 77 fsoinCR pic 9(2).
 
+                77 numeroAValide pic 9.
+                77 enclosNonExistant pic 9.
+                77 fdfA pic 9.
+                77 numA pic 9(3).
+                77 surnomA pic 9(30).
+                77 aNaissA pic 9(4).
+                77 especeA pic A(30).
+                77 numEnclosA pic 9(3).
+                77 frequenceRepasA pic 9(3).
+                77 dernierRepasA pic 9(9).
+                77 dernierVaccinA pic 9(9).
+
+                77 CE pic 9.
+                77 enclosComplet pic 9.
+                77 cptCE pic 9(3).
+
+
         PROCEDURE DIVISION.
+
+        AJOUT_ANIMAL.
+        OPEN I-O fanimaux
+        MOVE 0 TO numeroAValide
+        PERFORM WITH TEST AFTER UNTIL numeroAValide = 1
+             DISPLAY "Quel est l'identifiant de l'animal(entier)?"
+             ACCEPT numA
+             MOVE numA TO fa_numA
+             READ fanimaux
+                     INVALID KEY MOVE 1 TO numeroValide
+                     NOT INVALID KEY MOVE 0 TO numeroValide
+             END-READ
+        END-PERFORM
+        DISPLAY "Quel est son surnom?"
+        READ surnomA
+        PERFORM WITH TEST AFTER UNTIL aNaissA>1800 AND aNaissA<2019
+             DISPLAY "Quelle est son année de naissance?"
+             ACCEPT aNaissA
+        END-PERFORM
+        DISPLAY "Quel est son espece?"
+        READ especeA
+        MOVE 0 TO enclosNonExistant
+        MOVE 0 TO fdfA
+        OPEN INPUT fenclos
+        PERFORM WITH TEST AFTER UNTIL enclosNonExistant = 1 OR fdf=1
+        OR enclosComplet = 0
+        DISPLAY "Dans quel enclos est-il?"
+        READ numEnclosA
+        READ fenclos
+                  AT END
+                        MOVE 1 TO fdfA
+                  NOT AT END
+                        IF fa_numEnclos = numEnclosA THEN 
+                            MOVE 1 TO enclosNonExistant
+                        END-IF
+                  END-READ
+        MOVE numEnclosA TO CE
+        PERFORM CAPACITE_ENCLOS
+        IF enclosComplet = 1 THEN
+            DISPLAY "Cet enclos est complet"
+        END-IF
+        CLOSE fenclos
+        DISPLAY "Quel est sa fréquence de repas?"
+        READ frequenceRepasA
+        MOVE NULL to dernierRepasA
+        MOVE NULL to dernierVaccinA
+        OPEN OUTPUT fenclos
+            MOVE numA 
+
+FINIR L'AJOUT ANIMAL : move variable locale to tampon + faire fonction verif capacite enclos
+            
+         
+       CAPICITE_ENCLOS.
+       
+        
+
+
