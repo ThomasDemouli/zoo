1d47691ec793b065eaee307af8aa2c981cc9c1a6
Thomas DEMOULIN <E18B112Y@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-05 16:26:24 +0100

    iugvvg

---
+++ index.cob
@@ -0,0 +1,106 @@
+                IDENTIFICATION DIVISION.
+
+        PROGRAM-ID.parcZoologique.
+
+
+                ENVIRONMENT DIVISION.
+
+        INPUT-OUTPUT SECTION.
+        FILE-CONTROL.
+
+        SELECT fenclos ASSIGN TO "enclos.dat"
+        ORGANIZATION INDEXED
+        ACCESS MODE Dynamic
+        RECORD KEY fe_numE
+        ALTERNATE RECORD KEY fe_etat with duplicates
+        FILE STATUS is fenclCR.
+
+        SELECT fanimaux ASSIGN TO "animaux.dat"
+        ORGANIZATION INDEXED
+        RECORD KEY fa_numA
+        ALTERNATE RECORD KEY fa_numEnclos with duplicates
+        ACCESS MODE DYNAMIC
+        FILE STATUS is fanimCR.
+
+        SELECT femployes ASSIGN TO "employes.dat"
+        ORGANIZATION INDEXED
+        RECORD KEY fem_numS
+        ALTERNATE RECORD KEY fem_dateEmbauche with duplicates
+        ACCESS MODE DYNAMIC
+        FILE STATUS is femplCR.
+
+        SELECT frepas ASSIGN TO "repas.dat"
+        ORGANIZATION INDEXED
+        RECORD KEY fr_numR
+        ALTERNATE RECORD KEY fr_numAnimal with duplicates
+        ACCESS MODE DYNAMIC
+        FILE STATUS is frepaCR.
+
+        SELECT fsoins ASSIGN TO "soins.dat"
+        ORGANIZATION INDEXED
+        RECORD KEY fv_numS
+        ALTERNATE RECORD KEY fv_descriptif with duplicates
+        ALTERNATE RECORD KEY fv_numA with duplicates
+        ACCESS MODE DYNAMIC
+        FILE STATUS is fsoinCR.
+
+
+                DATA DIVISION.
+
+        FILE SECTION.
+        FD fenclos.
+                01 encl_tamp.
+                        02 fe_numE pic 9(3).
+                        02 fe_capacite pic 9(2).
+                        02 fe_etat pic A(25).
+
+        FD fanimaux.
+                01 anim_tamp.
+                        02 fa_numA pic 9(3).
+                        02 fa_surnom pic A(30).
+                        02 fa_anneeNaissance pic 9(4).
+                        02 fa_espece pic A(30).
+                        02 fa_numEnclos pic 9(3).
+                        02 fa_frequenceRepas pic 9(3).
+                        02 fa_dernierRepas pic 9(9).
+                        02 fa_dernierVaccin pic 9(9).
+
+        FD femployes
+                01 empl_tamp.
+                        02 fem_numEmp pic 9(4).
+                        02 fem_nom pic A(30).
+                        02 fem_prenom pic A(30).
+                        02 fem_dateNaissance pic A(10).
+                        02 fem_dateEmbauche pic A(10).
+                        02 fem_telephone pic 9(10).
+                        02 fem_type pic A(20).
+
+        FD frepas
+                01 repa_tamp
+                        02 fr_numR pic 9(9).
+                        02 fr_description pic A(999).
+                        02 fr_jour pic 9.
+                        02 fr_mois pic 9(2).
+                        02 fr_annee pic 9(4).
+                        02 fr_heure pic 9(4).
+                        02 fr_numSoigneur pic 9(4).
+                        02 fr_numAnimal pic 9(3).
+                        02 fr_prixRepas pic 9(4).
+
+        FD fsoins
+                01 soin_tamp
+                        02 fv_numS pic 9(4).
+                        02 fv_descriptif pic A(20).
+                        02 fv_date pic 9(8).
+                        02 fv_numMedecin pic A(30).
+                        02 fv_type pic A(30).
+                        02 fv_numA pic 9(4).
+
+        WORKING-STORAGE SECTION.
+                77 fenclCR pic 9(2).
+                77 fanimCR pic 9(2).
+                77 femplCR pic 9(2).
+                77 frepaCR pic 9(2).
+                77 fsoinCR pic 9(2).
+
+        PROCEDURE DIVISION.
