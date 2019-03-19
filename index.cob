efaae4800c5ab2b40ec20dec0c6b597f7f835ed1
Thomas DEMOULIN <E18B112Y@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-19 17:27:50 +0100

    ajout de toutes les fonctions

--- index.cob
+++ index.cob
@@ -740,6 +740,161 @@
       *                                   *
       *************************************
 
+        AJOUT_ANIMAL.
+        OPEN I-O fanimaux
+        MOVE 0 TO numeroAValide
+        PERFORM WITH TEST AFTER UNTIL numeroAValide = 1
+             DISPLAY "Quel est l'identifiant de l'animal(entier)?"
+             ACCEPT numA
+             MOVE numA to fa_numA
+             READ fanimaux
+                     INVALID KEY MOVE 1 TO numeroAValide
+                     NOT INVALID KEY MOVE 0 TO numeroAValide
+             END-READ
+        END-PERFORM
+        MOVE numA TO fa_numA        
+
+        DISPLAY "Quel est son surnom?"
+        ACCEPT surnomA
+        MOVE surnomA to fa_surnom
+
+        PERFORM WITH TEST AFTER UNTIL aNaissA>1800 AND aNaissA<2019
+             DISPLAY "Quelle est son année de naissance?"
+             ACCEPT aNaissA
+        END-PERFORM
+
+        MOVE aNaissA to fa_anneeNaissance 
+        DISPLAY "Quel est son espece?"
+        ACCEPT especeA
+        MOVE especeA to fa_espece
+
+        MOVE 0 TO enclosNonExistant
+        MOVE 0 TO fdfA
+        OPEN INPUT fenclos
+        PERFORM WITH TEST AFTER UNTIL enclosNonExistant = 1 OR fdfA=1
+        OR enclosComplet = 0
+            DISPLAY "Dans quel enclos est-il?"
+            ACCEPT numEnclosA
+            READ fenclos
+              AT END
+                    MOVE 1 TO fdfA
+              NOT AT END
+                IF fa_numEnclos = numEnclosA THEN 
+                    MOVE 1 TO enclosNonExistant
+                END-IF
+           END-READ     
+            MOVE numEnclosA TO CE
+            PERFORM CAPACITE_ENCLOS
+            IF enclosComplet = 1 THEN
+                DISPLAY "Cet enclos est complet"
+            END-IF
+        END-PERFORM
+        MOVE numEnclosA to fa_numEnclos
+        CLOSE fenclos
+        DISPLAY "Quel est sa fréquence de repas (en nombre de jours)?"
+        ACCEPT frequenceRepasA
+        MOVE frequenceRepasA to fa_frequenceRepas
+
+              
+        MOVE 0 to fa_dernierRepas
+        MOVE 0 to fa_dernierVaccin  
+
+        WRITE anim_tamp
+
+        END-WRITE
+        DISPLAY "Animal ajouté !"
+        CLOSE fanimaux.
+            
+      * procedure determinant si un enclos est complet ou pas
+       CAPACITE_ENCLOS.
+       OPEN INPUT fenclos
+       MOVE 0 TO enclosComplet
+       MOVE CE to fe_numE
+       READ fenclos
+            INVALID KEY DISPLAY "Cet enclos n'existe pas" 
+            NOT INVALID KEY MOVE fe_capacite TO capaciteEnclos
+       END-READ
+      *Ajouter un if enclos existe (pour enlever l'affichage enclos complet lorsque l'enclos n'existe pas)
+       CLOSE fenclos
+       MOVE CE TO fa_numEnclos
+       MOVE 0 TO fdf
+       START fanimaux, KEY IS = fa_numEnclos
+            INVALID KEY MOVE 0 TO enclosComplet
+            NOT INVALID KEY PERFORM WITH TEST AFTER UNTIL fdf=1
+                  READ fanimaux NEXT 
+                  AT END MOVE 1 TO fdf
+                  NOT AT END  ADD 1 TO cptCE
+                  END-READ 
+            END-PERFORM                     
+       END-START 
+       IF capaciteEnclos - cptCE <=0 THEN 
+                MOVE 1 TO enclosComplet
+       ELSE
+                MOVE 0 TO enclosComplet
+       END-IF.
+      
+       MODIF_ANIMAL.
+      *   OPEN I-O fanimaux
+      *  MOVE 0 TO numeroAValide
+      * PERFORM WITH TEST AFTER UNTIL numeroAValide = 1
+      * DISPLAY "Quel animal voulez vous modifié ? Entrez son identifiant"
+      *        ACCEPT numA
+      *       MOVE numA to fa_numA
+      *        READ fanimaux
+      *                INVALID KEY DISPLAY "Animal non existant"
+      *                NOT INVALID KEY MOVE 1 TO numeroAValide
+      *        END-READ
+      *  END-PERFORM
+      *  MOVE numA to fa_numA.
+      * lecture direct + demande des nouveaux attributs
+
+       SUPPRIMER_ANIMAL.
+       OPEN I-O fanimaux
+       DISPLAY "Entrez le numéro de l'animal à supprimer"
+       ACCEPT numA
+       MOVE numA TO fa_numA
+       READ fanimaux
+           INVALID KEY DISPLAY "L'animal n'existe pas"
+           NOT INVALID KEY 
+                DELETE fanimaux RECORD
+                DISPLAY "L'animal a été supprimé"
+       END-READ
+       CLOSE fanimaux.
+
+       AFFICHER_ANIMAL.
+       OPEN I-O fanimaux
+       DISPLAY "Entrez le numéro de l'animal à afficher"
+       ACCEPT numA
+       MOVE numA TO fa_numA
+       READ fanimaux
+           INVALID KEY DISPLAY "L'animal n'existe pas"
+           NOT INVALID KEY 
+                STRING "test" INTO descriptionA
+                DISPLAY descriptionA
+       END-READ
+       CLOSE fanimaux.
+       
+        AFFICHER_TOUS_LES_ANIMAUX.
+        OPEN INPUT fanimaux
+        MOVE 0 to fdf
+        PERFORM WITH TEST AFTER UNTIL fdf =1
+            READ fanimaux NEXT
+                AT END 
+                    DISPLAY "fin de fichier"
+                    MOVE 1 TO fdf
+                NOT AT END  
+                    STRING "ANIMAL n° " fa_numA ", Surnom :"
+                    fa_surnom ", Naissance :" fa_anneeNaissance
+                    ", Espece :" fa_espece ", Enclos :" fa_numEnclos
+                    ", Frequence repas : " fa_frequenceRepas
+                    ", Dernier vaccin : " fa_dernierVaccin
+                    ", Dernier repas : " fa_dernierRepas
+                    into descriptionA
+                    DISPLAY descriptionA
+            END-READ
+        END-PERFORM
+        CLOSE fanimaux.
+
 
 
 
@@ -750,6 +905,124 @@
       *                                    *
       **************************************
 
+        AJOUT_EMPLOYES.
+        OPEN i-o femployes
+        MOVE 0 TO idNonIdentique
+
+        PERFORM WITH TEST AFTER UNTIL idNonIdentique = 1
+            DISPLAY 'Donner un id à l employé'
+            ACCEPT wId
+            MOVE wId to fem_numEmp
+            READ femployes
+                INVALID KEY MOVE 1 TO idNonIdentique
+                NOT INVALID KEY DISPLAY "l'employé existe deja" 
+            END-READ
+           
+        END-PERFORM
+
+        DISPLAY 'Nom de l employe'
+        ACCEPT wNomEmpl
+        DISPLAY 'Prenom de l employe'
+        ACCEPT wPrenomEmpl
+        DISPLAY 'Date de naissance de l employé'
+        ACCEPT wDatenaissance
+        DISPLAY 'Date d embauche de l employé'
+        ACCEPT wDateEmbauche
+        DISPLAY 'Numéro de l employé'
+        ACCEPT wtelephone
+        DISPLAY 'Type de l employé'
+        ACCEPT wType
+ 
+        IF idNonIdentique = 1 THEN  
+            MOVE wId TO fem_numEmp
+            MOVE wNomEmpl TO fem_nom
+            MOVE wPrenomEmpl TO fem_prenom
+            MOVE wDatenaissance TO fem_dateNaissance
+            MOVE wDateEmbauche TO fem_dateEmbauche
+            MOVE wtelephone TO fem_telephone
+            MOVE wType TO fem_type
+            WRITE empl_tamp
+            END-WRITE
+        END-IF
+        CLOSE femployes.
+      
+            
+        MODIF_EMPLOYES.
+        OPEN i-o femployes
+        MOVE 0 TO idIdentique
+            PERFORM WITH TEST AFTER UNTIL idIdentique = 1
+                DISPLAY 'Donner l id de l employé à modifier '
+                ACCEPT wId
+                MOVE wId to fem_numEmp
+                READ femployes
+                INVALID KEY  DISPLAY "l'employé n'existe pas" 
+                NOT INVALID KEY MOVE 1 TO idIdentique 
+                END-READ
+            END-PERFORM
+
+          IF idIdentique = 1 THEN 
+            DISPLAY 'Que voulez vous modifier ?'
+            DISPLAY '1 : Son nom ? ?'
+            DISPLAY '2 : Son numéro de téléphone?'
+            DISPLAY '3 : Son type ?'
+
+            ACCEPT choix
+            EVALUATE  choix
+            WHEN "1" 
+	            DISPLAY 'Nouveau nom à l employé'
+                ACCEPT wNomEmpl
+                MOVE wNomEmpl to fem_nom
+            WHEN "2" 
+	            DISPLAY 'Nouveau numéro de tél de l employé'
+               ACCEPT wtelephone
+               MOVE wtelephone TO fem_telephone
+            WHEN "3" 
+	            DISPLAY 'Nouveau type à l employé'
+               ACCEPT wtype
+               MOVE wtype TO fem_type
+            END-EVALUATE
+        
+        END-IF
+        REWRITE empl_tamp
+        END-REWRITE
+        CLOSE femployes.
+
+        SUPPRESSION_EMPLOYES.
+        OPEN i-o femployes
+        MOVE 0 TO idIdentique
+            PERFORM WITH TEST AFTER UNTIL idIdentique = 1
+                DISPLAY 'Donner l id de l employes à supprimer '
+                ACCEPT wId
+                MOVE wId to fem_numEmp
+                READ femployes
+                INVALID KEY  DISPLAY "l'employé n'existe pas" 
+                NOT INVALID KEY MOVE 1 TO idIdentique 
+                END-READ
+            END-PERFORM
+
+        if idIdentique = 1 then
+            delete femployes record 
+        end-if
+        close femployes.
+
+        AFFICHAGE_EMPLOYES.
+        OPEN INPUT femployes
+        MOVE 0 TO wfin
+        PERFORM WITH TEST AFTER UNTIL wfin = 1
+                READ femployes next
+                AT END  
+                        MOVE 1 TO wfin
+                NOT AT END
+                        DISPLAY fem_numEmp
+                        DISPLAY fem_nom
+                        DISPLAY fem_prenom
+                        DISPLAY fem_dateNaissance  
+                        DISPLAY fem_dateEmbauche  
+                        DISPLAY fem_telephone
+                        DISPLAY fem_type
+                END-READ
+        END-PERFORM
+        CLOSE femployes.
 
 
 
@@ -759,3 +1032,141 @@
       *         FONCTIONS ENCLOS         *
       *                                  *
       ************************************
+
+        AJOUT_ENCLOS.
+        OPEN i-o fenclos
+        MOVE 0 TO idIdentique
+
+        PERFORM WITH TEST AFTER UNTIL idIdentique = 1
+            DISPLAY 'Donner un id à l enclos'
+            ACCEPT wId
+            MOVE wId to fe_numE
+            READ fenclos
+            INVALID KEY  MOVE 1 TO idIdentique
+            NOT INVALID KEY DISPLAY "l'enclos existe deja"
+              
+            END-READ
+        END-PERFORM
+        CLOSE fenclos
+        DISPLAY 'Donner une capacité à l enclos'
+        ACCEPT wCapEnclos
+        DISPLAY 'Etat de l enclos'
+        ACCEPT wEtatEnclos
+        CLOSE fenclos
+ 
+         IF idIdentique = 1 THEN 
+            OPEN i-o fenclos
+            MOVE wId TO fe_numE
+            MOVE wCapEnclos TO fe_capacite
+            MOVE wEtatEnclos TO fe_etat
+            WRITE encl_tamp
+            END-WRITE
+            CLOSE fenclos
+        END-IF.
+
+
+        MODIF_ENCLOS.
+        OPEN i-o fenclos
+        MOVE 0 TO idIdentique
+            PERFORM WITH TEST AFTER UNTIL idIdentique = 1
+                DISPLAY 'Donner l id de l enclos à modifier '
+                ACCEPT wId
+                MOVE wId to fe_numE
+                READ fenclos
+                INVALID KEY  DISPLAY "l'enclos n'existe pas" 
+                NOT INVALID KEY MOVE 1 TO idIdentique 
+                END-READ
+            END-PERFORM
+
+          IF idIdentique = 1 THEN 
+            DISPLAY 'Que voulez vous modifier ?'
+            DISPLAY '1 : Sa capacité ? ?'
+            DISPLAY '2 : Son etat'
+           
+
+            ACCEPT choix
+            EVALUATE  choix
+            WHEN "1" 
+	            DISPLAY 'Nouvelle capacité de l enclos'
+                ACCEPT wCapEnclos
+                MOVE wCapEnclos to fe_capacite
+            WHEN "2" 
+	            DISPLAY 'Nouvel etat de l enclos'
+               ACCEPT wEtatEnclos
+               MOVE wEtatEnclos TO fe_etat
+            
+            END-EVALUATE
+        
+        END-IF
+        REWRITE encl_tamp
+        END-REWRITE
+        CLOSE fenclos.
+
+        SUPPRESSION_ENCLOS.
+        OPEN i-o fenclos
+        MOVE 0 TO idIdentique
+            PERFORM WITH TEST AFTER UNTIL idIdentique = 1
+                DISPLAY 'Donner l id de l enclos à supprimer '
+                ACCEPT wId
+                MOVE wId to fe_numE
+                READ fenclos
+                INVALID KEY  DISPLAY "l'enclos n'existe pas" 
+                NOT INVALID KEY MOVE 1 TO idIdentique 
+                END-READ
+            END-PERFORM
+
+        if idIdentique = 1 then
+            delete fenclos record 
+        end-if
+        close fenclos.
+
+       
+
+        AFFICHAGE_ENCLOS.
+        OPEN INPUT fenclos
+        MOVE 0 TO wfin
+        PERFORM WITH TEST AFTER UNTIL wfin = 1
+                READ fenclos next
+                AT END  
+                        MOVE 1 TO wfin
+                NOT AT END
+                        DISPLAY fe_numE
+                        DISPLAY fe_capacite
+                        DISPLAY fe_etat
+                END-READ
+        END-PERFORM
+        CLOSE fenclos.
+
+        AFFICHER_EMPLOYES.
+        OPEN INPUT fenclos
+        MOVE 0 TO wfin
+        PERFORM WITH TEST AFTER UNTIL wfin = 1
+                READ fenclos next
+                AT END  
+                        MOVE 1 TO wfin
+                NOT AT END
+                        DISPLAY fe_numE
+                        DISPLAY fe_capacite
+                        DISPLAY fe_etat
+                END-READ
+        END-PERFORM
+        CLOSE fenclos.
+
+
+        AFFICHER_ENCLOS_ETAT.
+        OPEN INPUT fenclos
+        display "Vous voulez voir les enclos dans quel etat ?"
+        accept wEtatEnclos
+        PERFORM WITH TEST AFTER UNTIL wfin = 1
+                READ fenclos next
+                AT END  
+                        MOVE 1 TO wfin
+                NOT AT END
+                       if wEtatEnclos = fe_etat
+                          display "L enclos numéro "fe_numE" d une"
+                          display "capacité de" fe_capacite 
+                          display "est" fe_etat
+                                        
+                END-READ
+        END-PERFORM
+        close fenclos.
